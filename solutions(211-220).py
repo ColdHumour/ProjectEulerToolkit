45b5c67bb086a77eb7ca7df49f9d76a0a9f4c524
Jasper Wu <jasper.wuyd@gmail.com>
2015-10-15 18:35:10 +0800

    update 1015

--- solutions(211-220).py
+++ solutions(211-220).py
@@ -12,6 +12,7 @@ import math
 
 import ProjectEuler.combinatorics as pec
 import ProjectEuler.formulas as pef
+import ProjectEuler.generators as peg
 import ProjectEuler.prime as pep
 
 
@@ -57,3 +58,102 @@ def pe211(N=64000000):
 
     # answer: 1922364685
     return c_pe211(N)
+
+# %load_ext Cython
+
+# %%cython
+# import numpy as np
+# cimport numpy as np
+
+# def sum_segments(list segs):
+#     cdef unsigned short a, b, s, t
+#     cdef long tlens
+    
+#     if not segs:
+#         return 0
+    
+#     segs.sort()
+#     s, t = segs[0]
+#     tlens = t - s
+#     for a,b in segs[1:]:
+#         if a > t:
+#             tlens += b - a
+#             s, t = a, b
+#         elif b > t:
+#             tlens += b - t
+#             t = b
+#     return tlens
+
+# def parse_recs(list reclist):
+#     cdef dict idict = {}, pdict = {}, segs = {}
+#     cdef set endpoints = set()
+#     cdef unsigned short b, c, e, f, i, p
+#     cdef long v
+        
+#     for b, _, e, _, _ in reclist:
+#         endpoints.add(b)
+#         endpoints.add(b+e)
+    
+#     for i,p in enumerate(sorted(endpoints)):
+#         pdict[p] = i
+#         idict[i] = p
+    
+#     for b, c, e, f, _ in reclist:
+#         for i in range(pdict[b], pdict[b+e]):
+#             p = idict[i]
+#             if p in segs:
+#                 segs[p].append((c, c+f))
+#             else:
+#                 segs[p] = [(c, c+f)]
+    
+#     v = 0
+#     for i in range(len(idict)-1):
+#         p = idict[i]
+#         v += sum_segments(segs.get(p, [])) * (idict[i+1] - p)
+#     return v
+
+def pe212(N=50000):
+    """
+    Cut all cuboids into sub-cuboids with 1-width according to x-axis.
+    Then solve the overlapping rectangles problem on the y-z plane.
+    
+    Record all y-axis endpoints of rectangles, and then merge the sub- 
+    rectangles in each interval with same width.
+    
+    Cython provides huge savings on time and space. But still ~1.7G ~350s
+    Need to be carefully reviewed.
+    """
+    
+    def cube_gen():
+        sgen = peg.pe_lagged_fibo_generator(6)
+        while 1:
+            a, b, c, d, e, f = next(sgen)
+            a %= 10000
+            b %= 10000
+            c %= 10000
+            d = 1 + (d % 399)
+            e = 1 + (e % 399)
+            f = 1 + (f % 399)
+            yield a, b, c, d, e, f
+    
+    cg = cube_gen()
+    cubdict = {}
+    for _ in range(N):
+        a, b, c, d, e, f = next(cg)
+        cubdict.setdefault(a, []).append((b, c, e, f, d))
+    
+    a = v = 0
+    while a < 10399:
+        if a not in cubdict:
+            a += 1
+            continue
+
+        for b, c, e, f, d in cubdict[a]:
+            if d > 1:
+                cubdict.setdefault(a+1, []).append((b, c, e, f, d-1))
+
+        v += parse_recs(cubdict[a])
+        a += 1
+    
+    # answer: 328968937309
+    return v
