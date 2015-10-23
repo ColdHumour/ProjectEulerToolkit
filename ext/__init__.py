355282d460ce71dcdfb6b70fa03d457e52cc96c9
Jasper Wu <jasper.wuyd@gmail.com>
2015-10-23 10:58:14 +0800

    update 1023

--- ext/__init__.py
+++ ext/__init__.py
@@ -23,19 +23,22 @@ except:
 
     # build cython extensions
     os.chdir(EXT_DIR)
-    os.system('python extsetup.py build_ext -i --compiler=msvc')
+    state = os.system('python extsetup.py build_ext -i --compiler=msvc')
 
-    # clean intermediate files
-    build = os.path.join(EXT_DIR, 'build')
-    if os.path.exists(build):
-        shutil.rmtree(build)
-    for f in os.listdir(EXT_DIR):
-        faffix = f.split('.')
-        if faffix[-1] == 'c':
-            os.remove(os.path.join(EXT_DIR, f))
+    if state == 0:
+        # clean intermediate files
+        build = os.path.join(EXT_DIR, 'build')
+        if os.path.exists(build):
+            shutil.rmtree(build)
+        for f in os.listdir(EXT_DIR):
+            faffix = f.split('.')
+            if faffix[-1] == 'c':
+                os.remove(os.path.join(EXT_DIR, f))
 
-    # change back to workdir
-    os.chdir(CUR_DIR)
+        # change back to workdir
+        os.chdir(CUR_DIR)
 
-    import _formula
-    import _prime
+        import _formula
+        import _prime
+    else:
+        print "WARNING: Fail to build Cython extensions"
