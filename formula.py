f54aa6b276d8b4c00d52878092bf559efe68e039
Jasper Wu <jasper.wuyd@gmail.com>
2016-03-22 17:34:30 +0800

    update 0322

--- formula.py
+++ formula.py
@@ -5,7 +5,7 @@ formula.py
 
 Functions implementing formulas via fast algorithms.
 Function list:
-    sqrt, is_square, iroot, gcd, ggcd 
+    sqrt, is_square, iroot, gcd, ggcd
     fac, fac_mod, cprod
     sum_mod, pow_mod, iter_associate
     legendre_symbol
@@ -15,10 +15,10 @@ Function list:
     co_prime_tree
     stern_brocot_tree
 
-    rational_continous_frac, 
-    irrational_continous_frac, 
+    rational_continous_frac,
+    irrational_continous_frac,
     continous_frac_convergent
-    
+
 @author: Jasper Wu
 """
 
@@ -33,6 +33,8 @@ except:
     pow_mod = pow
     is_square = None
     fac = None
+    isqrt = None
+    iroot = None
 
 try:
     from . ext._formula import _c_sum_mod
@@ -41,7 +43,7 @@ except:
     sum_mod = None
 
 
-# Supplementry Implementations 
+# Supplementry Implementations
 def _is_square(n):
     """return whether n is a perfect square"""
 
@@ -51,12 +53,13 @@ def _is_square(n):
 if is_square is None:
     is_square = _is_square
 
+
 def _factorial(n):
     """return n!"""
 
     if n < 0:
         raise ValueError("n in n! must be positive!")
-    if n==0 or n==1: 
+    if n == 0 or n == 1:
         return 1
 
     output = 1
@@ -67,19 +70,25 @@ def _factorial(n):
 if fac is None:
     fac = _factorial
 
-def fac_mod(n, m):
-    """return n! % m"""
 
-    if n < 0:
-        raise ValueError("n in n! must be positive!")
-    if n==0 or n==1: 
-        return 1
+def _isqrt(n):
+    """return integer square root of n"""
 
-    output = 1
-    for i in xrange(2, n+1):
-        output *= i
-        output %= m
-    return output
+    return int(n**0.5)
+
+if isqrt is None:
+    isqrt = _isqrt
+
+
+def _iroot(n, m):
+    """return integer m-th root of n, and whether n is a perfect power"""
+
+    r = int(n**(1./m))
+    return r, r**m == n
+
+if isqrt is None:
+    iroot = _iroot
+
 
 # Useful Functions
 def cprod(seq):
@@ -90,9 +99,10 @@ def cprod(seq):
         output *= i
     return output
 
+
 def ggcd(seq):
     """
-    return the greatest common divisor (gcd) for n integers, 
+    return the greatest common divisor (gcd) for n integers,
     where n can larger than 2
     """
 
@@ -102,11 +112,14 @@ def ggcd(seq):
         return gcd(seq[0], seq[1])
     else:
         g = gcd(seq[-2], seq[-1])
-        if g == 1: return g
+        if g == 1:
+            return g
         for n in seq[:-2]:
             g = gcd(g, n)
-            if g == 1: return 1
+            if g == 1:
+                return 1
         return g
+
 
 def padic(n, p, ntype='s'):
     """change integer n from base 10 to base p"""
@@ -114,7 +127,8 @@ def padic(n, p, ntype='s'):
     base = '0123456789' + ''.join(map(chr, range(65, 92)))
     snp = ''
     while True:
-        if n == 0: break
+        if n == 0:
+            break
         n, r = divmod(n, p)
         snp += base[r]
 
@@ -125,6 +139,7 @@ def padic(n, p, ntype='s'):
         return int(snp)
     elif ntype == 'l':
         return list(snp)
+
 
 def max_subarray(array):
     """return max sum of any continous subarray of an array"""
@@ -139,7 +154,7 @@ def max_subarray(array):
 # Modulo Functions
 def _sum_mod(n):
     """return n%2 + n%3 + ... + n%(n-1)"""
-    
+
     from itertools import takewhile, count
 
     sm = i = 0
@@ -148,11 +163,27 @@ def _sum_mod(n):
         b = n % (n/i) if i > 1 else 1
         c = (a-b) / i + 1
         sm += b*c + i*(c - 1)*c / 2
-    sm += sum(n%j for j in xrange(2, n/(i+1) + 1))
+    sm += sum(n % j for j in xrange(2, n/(i+1) + 1))
     return sm
 
 if sum_mod is None:
     sum_mod = _sum_mod
+
+
+def fac_mod(n, m):
+    """return n! % m"""
+
+    if n < 0:
+        raise ValueError("n in n! must be positive!")
+    if n == 0 or n == 1:
+        return 1
+
+    output = 1
+    for i in xrange(2, n+1):
+        output *= i
+        output %= m
+    return output
+
 
 def iter_associate(f, x, n):
     """
@@ -167,6 +198,7 @@ def iter_associate(f, x, n):
         n /= 2
         x = f(x, x)
     return r
+
 
 def legendre_symbol(a, p):
     """
@@ -184,7 +216,7 @@ def legendre_symbol(a, p):
 # Tree Generation
 def pythag_triple_tree(triple=(3, 4, 5), forward=True, trust=False):
     """
-    Primitive Pythagorean Triple (PPT) (a, b, c) is integer triple satisfying 
+    Primitive Pythagorean Triple (PPT) (a, b, c) is integer triple satisfying
         a**2 + b**2 = c**2
         gcd(a, b) = gcd(b, c) = gcd(a, c) = 1
     Given a PPT, it can generate three more PPTs. And if recursively applying this branching function from (3, 4, 5), all PPTs in form (odd, even, odd) can be generated in a trinary tree, which covers the entire set of PPTs completely and uniquely.
@@ -200,8 +232,8 @@ def pythag_triple_tree(triple=(3, 4, 5), forward=True, trust=False):
             raise ValueError("Invalid Primitive Pythagorean Triple")
 
     if forward:
-        return (( a-2*b+2*c,  2*a-b+2*c,  2*a-2*b+3*c),
-                ( a+2*b+2*c,  2*a+b+2*c,  2*a+2*b+3*c),
+        return ((a-2*b+2*c,   2*a-b+2*c,  2*a-2*b+3*c),
+                (a+2*b+2*c,   2*a+b+2*c,  2*a+2*b+3*c),
                 (-a+2*b+2*c, -2*a+b+2*c, -2*a+2*b+3*c))
     else:
         if triple == (3, 4, 5):
@@ -209,9 +241,10 @@ def pythag_triple_tree(triple=(3, 4, 5), forward=True, trust=False):
         else:
             return (abs(-a-2*b+2*c), abs(-2*a-b+2*c), -2*a-2*b+3*c)
 
+
 def co_prime_tree(pair=(0, 0), trust=False):
     """
-    All co-prime pairs can be generated from (2, 1) (for (odd, even) and (even, odd) pairs) and (3, 1) (for (odd, odd) pairs). 
+    All co-prime pairs can be generated from (2, 1) (for (odd, even) and (even, odd) pairs) and (3, 1) (for (odd, odd) pairs).
     It follows a trinary tree, from co-prime pair (a, b), we get: (2*a - b, a), (2*a + b, a), (a + 2*b, b)
     It can be shown that the co-prime pairs in the tree are disjoint complete.
     """
@@ -222,8 +255,9 @@ def co_prime_tree(pair=(0, 0), trust=False):
     a, b = pair
     if not trust:
         if gcd(a, b) != 1:
-            raise ValueError("Invalid co-prime pair!")            
+            raise ValueError("Invalid co-prime pair!")
     return ((2*a - b, a), (2*a + b, a), (a + 2*b, b))
+
 
 def stern_brocot_tree():
     """
@@ -245,49 +279,52 @@ def rational_continous_frac(p, q=1, limit=100):
     """
 
     p, q, cfrac = int(p), int(q), []
-    while p%q:
-        print p, q
+    while p % q:
         cfrac.append(p/q)
-        p, q = q, p%q
+        p, q = q, p % q
     if q:
         cfrac.append(p)
     return tuple(cfrac)
+
 
 def irrational_continous_frac(d, p=0, q=1, limit=100):
     """
     return continued fraction expansion of quadratic irrational number (p + sqrt(d)) / q
     about quadratic irrational number: https://en.wikipedia.org/wiki/Quadratic_irrational
     """
-    
+
+    from fractions import Fraction
+
     d, p, q = int(d), int(p), int(q)
-    
+
     sd = sqrt(d)
     if int(sd) * int(sd) == d:
         raise ValueError("D is perfect square!")
-    
+
     a = int((p + sd) / q)
     repetend, pairspq = [a], []
     for _ in xrange(limit):
         p = Fraction(a*q - p, 1)
         q = Fraction(d - p*p, q)
         a = int((p + sd) / q)
-        
+
         if (p, q) in pairspq:
             i = pairspq.index((p, q))
             return tuple(repetend[:i+1] + [tuple(repetend[i+1:])])
-        
+
         pairspq.append((p, q))
         repetend.append(a)
     raise ValueError("Repetend is longer than {0:d}, please try higher limit!".format(limit))
 
+
 def continous_frac_convergent(cfrac):
     """
-    given continued fraction, return series of convergents 
+    given continued fraction, return series of convergents
     """
 
     if len(cfrac) < 2:
         raise ValueError("Continued fraction must longer than 2!")
-    
+
     a0, a1 = cfrac[:2]
     p0, p1, q0, q1 = a0, a0*a1+1, 1, a1
     cvg = [Fraction(p0, q0), Fraction(p1, q1)]
